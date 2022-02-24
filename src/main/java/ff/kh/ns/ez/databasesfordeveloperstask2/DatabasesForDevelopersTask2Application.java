bbf9b9a7a8a74e34a865a94a84188879f6516a74
Ficko05 <filip-f@hotmail.com>
2022-02-24 14:28:15 +0100

    first push

---
+++ src/main/java/ff/kh/ns/ez/databasesfordeveloperstask2/DatabasesForDevelopersTask2Application.java
@@ -0,0 +1,13 @@
+package ff.kh.ns.ez.databasesfordeveloperstask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesForDevelopersTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasesForDevelopersTask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ff/kh/ns/ez/databasesfordeveloperstask2/DatabasesForDevelopersTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ff.kh.ns.ez.databasesfordeveloperstask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesForDevelopersTask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
